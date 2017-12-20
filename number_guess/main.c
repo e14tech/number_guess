f3eccf51e25239701e881c7284d59bc2d9e34f8d
Joshua Ernzen <josh@elem14.tech>
2017-12-20 12:51:23 -0600

    Initial Commit

---
+++ number_guess/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  number_guess
+//
+//  Created by Joshua Ernzen on 12/20/17.
+//  Copyright © 2017 Joshua Ernzen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
